b73241f0358ed2738f9ac4485df0e0a3b2c1b829
Dariusz Danielewski <dydek@hotmail.com>
2024-01-20 17:13:14 -0500

    Seed

---
+++ src/main/java/org/example/demowebfluxgraphql/DemoWebfluxGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.demowebfluxgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebfluxGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebfluxGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demowebfluxgraphql/DemoWebfluxGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demowebfluxgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebfluxGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
