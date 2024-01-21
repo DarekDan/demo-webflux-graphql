21e14cd47de56c1d7da15f153146b315d53ac9ad
Dariusz Danielewski <dydek@hotmail.com>
2024-01-20 20:33:36 -0500

    Prep for filter implementation

--- src/main/java/org/example/demowebfluxgraphql/repositories/BookRepository.java
+++ src/main/java/org/example/demowebfluxgraphql/repositories/BookRepository.java
@@ -1,10 +1,9 @@
 package org.example.demowebfluxgraphql.repositories;
 
 import org.example.demowebfluxgraphql.entities.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
