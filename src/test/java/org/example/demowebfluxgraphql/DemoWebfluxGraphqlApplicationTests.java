30f80c620e5dc19677e2c748f5b3b9ad3c3d3ea1
Dariusz Danielewski <dydek@hotmail.com>
2024-01-22 15:17:08 -0500

    Easier way to configure WebTestClient

--- src/test/java/org/example/demowebfluxgraphql/DemoWebfluxGraphqlApplicationTests.java
+++ src/test/java/org/example/demowebfluxgraphql/DemoWebfluxGraphqlApplicationTests.java
@@ -1,45 +1,30 @@
 package org.example.demowebfluxgraphql;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.example.demowebfluxgraphql.builders.GraphQLQueryBuilder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 @Slf4j
 class DemoWebfluxGraphqlApplicationTests {
 
-    @LocalServerPort
-    private int port;
-
+    @Autowired
     WebTestClient webTestClient;
 
     @Autowired
-    ApplicationContext context;
-
-    @Autowired
     GraphQLQueryBuilder builder;
-
-    @BeforeEach
-    void setUp(){
-        webTestClient = WebTestClient.bindToApplicationContext(context)
-                .configureClient().baseUrl("http://localhost:" + port)
-                .build();
-    }
 
     @Test
     void contextLoads() {
         // Test to ensure the application context loads successfully
     }
 
-    @SneakyThrows
     @Test
     void testGraphQLQueryFindAllBooks() {
         builder.query("books",  "id title author");
